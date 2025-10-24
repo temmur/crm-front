dbbbdb0bd4430931b3a1f3c4307014531383aba6
temmur <143727496+temmur@users.noreply.github.com>
2025-10-24 15:02:48 +0500

    added i18n

--- src/components/Common/CHeader.vue
+++ src/components/Common/CHeader.vue
@@ -9,10 +9,28 @@
     <div class="flex items-center space-x-4">
       <CButton text="Sign In" variant="light" button-class="mr-2!"/>
       <CButton text="Sign Up" variant="dark"/>
+      <div class="p-4">
+        <CLangSwitcher v-model="selectedLang"
+                       :languages="availableLanguages" />
+      </div>
     </div>
   </header>
 </template>
 <script setup lang="ts">
 import CButton from "@/components/Form/CButton.vue";
 import CSearch from "@/components/Form/CSearch.vue";
+import CLangSwitcher from "@/components/UI/CLangSwitcher.vue";
+import { useI18n } from 'vue-i18n'
+
+import {ref} from "vue";
+
+const selectedLang = ref(localStorage.getItem('locale') || 'en')
+
+const availableLanguages = [
+  { code: 'en', label: 'English' },
+  { code: 'ru', label: 'Русский' },
+  { code: 'uz', label: "O'zbekcha" },
+
+]
+const { t } = useI18n()
 </script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import Aura from '@primevue/themes/aura'   // ✅ this is the theme preset
 import Lara from '@primevue/themes/lara'
 import ToastService from 'primevue/toastservice'
 import 'primeicons/primeicons.css'
+import i18n from "@/i18n/i18n.js";
 // createApp(App).use(router).use(PrimeVue).mount('#app')
 
 const app = createApp(App)
@@ -19,4 +20,5 @@ app.use(PrimeVue, {
     }
 })
 app.use(ToastService)
+app.use(i18n)
 app.mount('#app')
